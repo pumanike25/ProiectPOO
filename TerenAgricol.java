0aec047d5b13bece6c267ec0c4b8c69c9f8b836d
Dorian003 <149613192+Dorian003@users.noreply.github.com>
2023-11-21 09:27:55 +0200

    Update TerenAgricol.java

--- TerenAgricol.java
+++ TerenAgricol.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.imobiliareproiect;
-
-
+package com.mycompany.imobiliarepro;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 
 public class TerenAgricol extends Imobiliare{
@@ -20,7 +15,7 @@ public class TerenAgricol extends Imobiliare{
     private long taxa;
     
     
-    public TerenAgricol(long pretVanzare, int suprafata, String judet, String adresa, String localitate, String tipSolicitant, String tipCultura, String tipSol, boolean irigatii, String clasificare) 
+    public TerenAgricol(long pretVanzare, int suprafata, String localitate, String judet, String adresa, String tipSolicitant, String tipCultura, String tipSol, boolean irigatii, String clasificare) 
     {
     super(pretVanzare,suprafata,localitate,judet,adresa);
     this.tipSolicitant = tipSolicitant;
@@ -41,7 +36,7 @@ public class TerenAgricol extends Imobiliare{
 
     public TerenAgricol(TerenAgricol a)
     {
-        super(a.getPretVanzare(), a.getSuprafata(), a.getLocalitate(),a.getJudet(), a.getAdresa());
+        super(a.getPretVanzare(), a.getSuprafata(),a.getLocalitate() ,a.getJudet(), a.getAdresa());
         this.tipCultura=a.tipCultura;
         this.tipSol=a.tipSol;
         this.tipSolicitant=a.tipSolicitant;
@@ -65,8 +60,8 @@ public class TerenAgricol extends Imobiliare{
     
     public String toString() {
         return "Terenul agricol cu suprafata de: " + this.getSuprafata() +" metri patrati" +"("+this.m2ha()+" ha"+")"+", cu pretul de: "+this.getPretVanzare() 
-                +" RON" + ", in locatia: "+this.getLocalitate() +"\n"+"Cu tipul de cultura: "+this.tipCultura
-                +",  tipul solicitantului: "+this.tipSolicitant+", tipul solului: "+this.tipSol+", are irigatii: " + (this.irigatii ? "Da" : "Nu")+"\n"+"Clasificare: "+this.clasificare+"\n";
+                +" RON" + "; In judetul: " + this.getJudet()+" ,localitatea: "+this.getLocalitate()+" ,adresa: "+this.getAdresa()+"\n"+"Cu tipul de cultura: "+this.tipCultura
+                +",  tipul solicitantului: "+this.tipSolicitant+", tipul solului: "+this.tipSol+", are irigatii: " + (this.irigatii ? "Da" : "Nu")+"\n"+"Clasificare: "+this.clasificare;
     }
     
     
@@ -147,7 +142,62 @@ public class TerenAgricol extends Imobiliare{
     }   
     
     
-    
+    public static void main(String[] args) {
+        //String numeFisier = "test_teren_constructii.txt";
+        
+        
+        
+        // Inițializarea 3 obiecte TerenAgricol distincte
+        TerenAgricol tr1,tr2,tr3;
+        tr1=new TerenAgricol(50000,57500,"Cavadinesti","Galati","Drum Agricol 1","fermier", "teren arabil","cernoziom",true,"extravilan");
+        tr2=new TerenAgricol(50000,57500,"Onesti","Galati","Drum Forestier 5","investitor", "pasune","cernoziom",true,"extravilan");
+        tr3=new TerenAgricol(50000,57500,"Cavadinesti","Galati","Drum Agricol 7","cooperativa agricola", "helesteu","luciu de apa",true,"extravilan");
+        
+        
+         // Scriere în fișier
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("teren_agricol.txt"))) 
+        {
+            writer.write("Informații despre terenul 1:\n");
+            writer.write(tr1.toString());
+            writer.write("\n\nInformații despre terenul 2:\n");
+            writer.write(tr2.toString());
+            writer.write("\n\nInformații despre terenul 3:\n");
+            writer.write(tr3.toString());
+            System.out.println("Informațiile despre terenurile agricole au fost scrise în fișierul terenuri_agricole.txt.");
+        } 
+        catch (IOException e) 
+        {
+            System.out.println("Eroare la scrierea în fișier: " + e.getMessage());
+        }
+        
+        
+        
+        TerenAgricol[] vta = new TerenAgricol[10];
+
+        // Inițializarea vectorului cu obiecte TerenAgricol distincte
+        vta[0]= new TerenAgricol(50000,57500,"Cavadinesti","Galati","Drum Agricol 1","fermier", "teren arabil","cernoziom",true,"extravilan");
+        vta[1]= new TerenAgricol(180000,407500,"Cavadinesti","Galati","Drum forestier 1","autoritate locala", "helesteu","luciu apa",true,"extravilan");
+        vta[2]= new TerenAgricol(180000,407500,"Cavadinesti","Galati","Drum Agricol 1","investitor", "teren arabil","argilos",true,"extravilan");
+        vta[3]= new TerenAgricol(150000, 80000, "Braila", "Braila", "Drum forestier 2", "fermier", "livada", "argilos", false, "intravilan");
+        vta[4]= new TerenAgricol(200000, 95000, "Bacau", "Bacau", "Drum Agricol 3", "cooperativa agricola", "teren arabil", "sandy", true, "extravilan");
+        vta[5]= new TerenAgricol(120000, 60000, "Sibiu", "Sibiu", "Drum Agricol 4", "fermier", "pasune", "argilos", false, "intravilan");
+        vta[6]= new TerenAgricol(250000, 120000, "Constanta", "Constanta", "Drum Agricol 5", "cooperativa agricola", "teren arabil", "cernoziom", true, "extravilan");
+        vta[7]= new TerenAgricol(180000, 85000, "Pitesti", "Arges", "Drum forestier 6", "fermier", "teren arabil", "sandy", false, "intravilan");
+        vta[8]= new TerenAgricol(300000, 150000, "Craiova", "Dolj", "Drum forestier 7", "investitor", "livada", "argilos", true, "extravilan");
+        vta[9]= new TerenAgricol(200000, 100000, "Targu Jiu", "Gorj", "Drum  8", "autoritate locala", "teren arabil", "cernoziom", false, "intravilan");
+
+        
+        // Afișarea detaliilor pentru fiecare teren agricol din vector
+        for (int i = 0; i < vta.length; i++) {
+            System.out.println("Detalii Teren Agricol " + (i + 1) + ":");
+            System.out.println(vta[i].toString());
+            System.out.println();
+        }
+        
+        
+       
+       
+    }
     
     
 }

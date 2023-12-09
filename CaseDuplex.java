93f2bc7d9cd59aae32d5a008701e7c79d3066c4f
TuskyFacLinii <149613557+TuskyFacLinii@users.noreply.github.com>
2023-12-10 01:23:31 +0200

    CaseDuplex.java

--- CaseDuplex.java
+++ CaseDuplex.java
@@ -15,17 +15,10 @@ import java.io.FileReader;
 public class CaseDuplex extends Case {
     private boolean esteImpartitaInDoua;
 
-    /*public CaseDuplex(){
-        super(0,0,null,null,null,0,0,0,0,0,0,0,0,0,0,false,false,false,false,false,false,false,false,null);
-        this.esteImpartitaInDoua = false;
-    }   
-  }*/
-    
     public CaseDuplex(){
         super(0,0,null,null,null,0,0,0,0,0,0,0,0,0,0,false,false,false,false,false,false,false,false,null);
         this.esteImpartitaInDoua = false;
     }   
-    
     // Constructor explicit
     public CaseDuplex(long pretVanzare, int suprafata, String localitate, String judet, String adresa, int suprafataUtila, int suprafataTeren, int nrCamere, int nrBucatarii,
             int nrBalcoane, int nrTerase, int nrLocuriParcare,int nrBai, int etaj, int anConstructie, boolean piscina, boolean beci, boolean izolatieTermica, boolean curent,
@@ -126,3 +119,12 @@ public class CaseDuplex extends Case {
     
     
     } 
+
+
+
+
+
+
+
+
+
